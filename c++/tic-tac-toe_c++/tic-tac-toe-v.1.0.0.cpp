59a20868438062692229c5703cb00247946c9d2c
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-16 21:55:16 +0700

    -

--- c++/tic-tac-toe_c++/tic-tac-toe-v.1.0.0.cpp
+++ c++/tic-tac-toe_c++/tic-tac-toe-v.1.0.0.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 int input_index(void)
 {
-    int index;
-    cin >> index;
+    cout << "Select location you want to place : ";
+    int index; cin >> index; return index;
+}
+
+// index can not replacement
+int check_index_used(int index)
+{
+    vector<int> index_used;
+    for (int i = 0; i < 9; i++)
+    {
+    if (index == index_used[i])
+    {
+        cout << "You can not use same location" << endl;
+        index = input_index();
+        break;
+    }
+    else
+    {
+        index_used.push_back(index);
+    }
+    }
     return index;
 }
 
@@ -24,7 +43,7 @@ int main()
 
     // use interface section
     cout << "TIC-TAC-TOE Game 2 player" << endl;
-    cout << "number showing on screen use for location you want to place x or o" << endl;
+    cout << "Number showing on screen use for location you want to place x or o" << endl;
     cout << "Example    " ;
     cout << "1  |  2  |  3" << endl;
     cout << "           4  |  5  |  6" << endl;
@@ -32,38 +51,22 @@ int main()
     cout << "**************************************************************" << endl;
 
     // section input from user
-    int index = 0; vector<int> index_used; char a[9] = {'1','2','3','4','5','6','7','8','9'};
+    int index = 0; char a[9] = {'1','2','3','4','5','6','7','8','9'};
     char used;
     for (int i = 1; i <= 9; i++)
     {
         // for user interface
         if (i % 2 == 0)
         {
-            cout << "Player 1" << endl;
-            used = 'x';
+            cout << "Player 2 : "; used = 'x';
         }
         else
         {
-            cout << "Player 2" << endl;
-            used = 'o';
-        }
-        cout << "Select location you want to place : ";
-        
-        // index can not replacement
-        cin >> index;
-        for (int i = 0; i < 9; i++)
-        {
-            if (index == index_used[i])
-            {
-                cout << "You can not use same location" << endl;
-                break;
-            }
-            else
-            {
-                index_used.push_back(index);
-            }
+            cout << "Player 1 : "; used = 'o';
         }
         
+        index = input_index(); index = check_index_used(index);
+
         cout << output_tic_tac_toe(index, a, used) << endl;
     }
     
