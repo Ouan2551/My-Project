11d4801bdcc256a0ec60095c203a8a5621916fe9
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-16 14:28:23 +0700

    -

--- c++/tic-tac-toe_c++/tic-tac-toe-v.1.0.0.cpp
+++ c++/tic-tac-toe_c++/tic-tac-toe-v.1.0.0.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+string output_tic_tac_toe(void)
+{
+    cout << "x  |  x  |  x" << endl;
+    cout << "o  |  x  |  o" << endl;
+    cout << "x  |  x  |  x" << endl;
+    return 0;
+}
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    string x = output_tic_tac_toe();
+    cout << x;
     return 0;
 }
