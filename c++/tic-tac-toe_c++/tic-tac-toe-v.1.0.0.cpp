6e5e903efd94dfce5b2db29e2516180c486d4f0c
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-16 21:23:22 +0700

    -

--- c++/tic-tac-toe_c++/tic-tac-toe-v.1.0.0.cpp
+++ c++/tic-tac-toe_c++/tic-tac-toe-v.1.0.0.cpp
@@ -1,16 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-string output_tic_tac_toe(void)
+string output_tic_tac_toe(int index, char a)
 {
-    cout << "x  |  x  |  x" << endl;
-    cout << "o  |  x  |  o" << endl;
-    cout << "x  |  x  |  x" << endl;
+    
+    for (int i = 1; i <= 9; i = i + 3)
+    {
+        cout << a[i] << "|" << a[i+1] << "|" << a[i+2] << endl;
+    }
+    
     return 0;
 }
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    string x = output_tic_tac_toe();
-    cout << x;
+
+    // use interface section
+    cout << "TIC-TAC-TOE Game 2 player" << endl;
+    cout << "number showing on screen use for location you want to place x or o" << endl;
+    cout << "Example    " ;
+    cout << "1  |  2  |  3" << endl;
+    cout << "           4  |  5  |  6" << endl;
+    cout << "           7  |  8  |  9" << endl; cout << endl;
+    cout << "**************************************************************" << endl;
+
+    // section input from user
+    int index = 0; vector<int> index_used; char a[9] = {'1','2','3','4','5','6','7','8','9'};
+    for (int i = 1; i <= 9; i++)
+    {
+        // for user interface
+        if (i % 2 == 0)
+        {
+            cout << "Player 1" << endl;
+        }
+        else
+        {
+            cout << "Player 2" << endl;
+        }
+        cout << "Select location you want to place : " << endl;
+        
+        // index can not replacement
+        cin >> index;
+        for (int i = 0; i < 9; i++)
+        {
+            if (index == index_used[i])
+            {
+                cout << "You can not use same location" << endl;
+            }
+            else
+            {
+                index_used.push_back(index);
+            }
+        }
+
+        cout << output_tic_tac_toe(index, a[9]) << endl;
+    }
+    
     return 0;
 }
+
+//this version start from player 1 only
