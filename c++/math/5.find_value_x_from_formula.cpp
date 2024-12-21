8e7c6f6ce48341d684edf6b1eb903a0a59977f15
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-21 23:48:43 +0700

    -

--- c++/math/5.find_value_x_from_formula.cpp
+++ c++/math/5.find_value_x_from_formula.cpp
@@ -4,6 +4,42 @@ int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     //INPUT ax^2 + bx + c OUTPUT value of x
+    string formula; getline(cin, formula); int m = formula.size();
+    vector<char> txt;
+    for (int i = 0; i < m; i++)
+    {
+        txt.push_back(formula[i]);
+    }
+
+    //find a
+    for (int i = 0; i < m; i++)
+    {
+        if (txt[i] == 'x' && txt[i+1] == '^' && txt[i+2] == '2')
+        {
+            char a = txt[1];
+            cout << "a" << endl;
+        }
+    }
+
+    //find b
+    for (int i = 0; i < m; i++)
+    {
+        if (txt[i] == 'x' && txt[i+1] != '^' && txt[i+2] != '2')
+        {
+            char b = txt[i-1];
+            cout << "b" << endl;
+        }
+        
+    }
+
+    //find c
+    for (int i = 0; i < m; i++)
+    {
+        
+    }
     
     return 0;
 }
+
+//idea
+//have mode select real number and complex numbers
