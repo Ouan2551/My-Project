d3bf03d717cb439843fd0eaa916a5323739bacd4
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-22 10:35:14 +0700

    -

--- c++/math/5.find_value_x_from_formula.cpp
+++ c++/math/5.find_value_x_from_formula.cpp
@@ -5,18 +5,29 @@ int main()
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     //INPUT ax^2 + bx + c OUTPUT value of x
     string formula; getline(cin, formula); int m = formula.size();
-    vector<char> txt;
+    vector<char> txt; char a, b, c;
     for (int i = 0; i < m; i++)
     {
         txt.push_back(formula[i]);
     }
+
+    // clear space bar in this vector
+    for (int i = 0; i < m; i++)
+    {
+        if (txt[i] == ' ')
+        {
+            
+        }
+        
+    }
+    
 
     //find a
     for (int i = 0; i < m; i++)
     {
         if (txt[i] == 'x' && txt[i+1] == '^' && txt[i+2] == '2')
         {
-            char a = txt[1];
+            a = txt[0];
             cout << "a" << endl;
         }
     }
@@ -26,18 +37,31 @@ int main()
     {
         if (txt[i] == 'x' && txt[i+1] != '^' && txt[i+2] != '2')
         {
-            char b = txt[i-1];
+            b = txt[i-1];
             cout << "b" << endl;
         }
         
     }
 
+    //ax^2 + bx + c
     //find c
     for (int i = 0; i < m; i++)
     {
-        
+        int chk_plus = 0;
+        if (txt[i] == '+')
+        {
+            chk_plus++;
+            if (chk_plus == 2)
+            {
+                c = txt[13];
+            }
+            
+        }
     }
     
+    cout << "a : " << a << endl;
+    cout << "b : " << b << endl;
+    cout << "c : " << c << endl;
     return 0;
 }
 
