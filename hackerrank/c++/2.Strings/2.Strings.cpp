ce94ef52ee06210c8efcb120d6dc28c1d18e17b1
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-25 18:14:31 +0700

    -

--- hackerrank/c++/2.Strings/2.Strings.cpp
+++ hackerrank/c++/2.Strings/2.Strings.cpp
@@ -3,25 +3,35 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    //setup zone
     string txt[2] = {""};
     for (int i = 0; i < 2; i++)
     {
         cin >> txt[i];
     }
+
+    // string txt1 = txt[0], txt2 = txt[1];
     int size_1 = txt[0].size(), size_2 = txt[1].size();
-    int size[2] = {size_1, size_2}; vector <string> all_consonant;
+    int size[2] = {size_1, size_2}; vector <char> all_consonant;
 
     //add all consonant
     for (int i = 0; i < 2; i++)
     {
-        for (int j = 0; j < size[i]; i++)
+        string txt_use = txt[i];
+        for (int j = 0; j < size[i]; j++)
         {
-            // all_consonant.push_back(txt[2][j]);
+            all_consonant.push_back(txt_use[j]);
         }
     }
-    
+
     //output
     cout << size[0] << " " << size[1] << endl;
-
+    for (int i = 0; i < size[0] + size[1]; i++)
+    {
+        cout << all_consonant[i];
+    }
+    cout << endl;
+    
     return 0;
 }
