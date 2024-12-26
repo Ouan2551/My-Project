15cb6d869fa768d4ff7df6880559431f5e1cdb2b
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-26 13:54:49 +0700

    -

--- hackerrank/c++/1.Introduction/8.Variable Sized Arrays.cpp
+++ hackerrank/c++/1.Introduction/8.Variable Sized Arrays.cpp
@@ -1,29 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+int array1(int array_index, int a[])
+{
+    int num = a[array_index];
+    return num;
+}
+int array2(int array_index, int b[])
+{
+    int num = b[array_index];
+    return num;
+}
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int n = 0, q = 0; cin >> n >> q;
-    int size1 = 0, size2 = 0, a[size1][size2] = {0};
-    cin >> size1;
-    for (int i = 0; i < size1; i++)
+    int n, q, k, m; cin >> n >> q; cin >> k; int a[k];
+    for (int i = 0; i < k; i++)
     {
-        cin >> a[0][i];
+        cin >> a[i];
     }
-    cin >> size2;
-    for (int i = 0; i < size2; i++)
+    cin >> m; int b[m];
+    for (int i = 0; i < m; i++)
     {
-        cin >> a[1][i];
+        cin >> b[i];
     }
+    int select_array1, array_index1,select_array2, array_index2;
+    cin >> select_array1 >> array_index1 >> select_array2 >> array_index2;
 
-    int select_array1 = 0, pointer1 = 0;
-    cin >> select_array1; cin >> pointer1;
-
-    int select_array2 = 0, pointer2 = 0;
-    cin >> select_array2; cin >> pointer2;
-    
-    cout << a[select_array1][pointer1] << endl;
-    cout << a[select_array2][pointer2] << endl;
-    
+    if (select_array1 == 0)
+    {
+        cout << array1(array_index1, a) << endl;
+    }
+    else if (select_array1 == 1)
+    {
+        cout << array2(array_index1, b) << endl;
+    }
+    if (select_array2 == 0)
+    {
+        cout << array1(array_index2, a) << endl;
+    }
+    else if (select_array2 == 1)
+    {
+        cout << array2(array_index2, b) << endl;
+    }
     return 0;
 }
+
+// Sample Input
+
+// 2 2 (i,j)
+// 3 ,1 5 4 (size_array1, number contain in arrays1)
+// 5 ,1 2 8 9 3 (size_array2, number contain in arrays2)
+// 0 ,1 (select array1, find character is index equal this number)
+// 1 ,3 (select array2, find character is index equal this number)
--- hackerrank/c++/2.Strings/1.StringStream.cpp
+++ hackerrank/c++/2.Strings/1.StringStream.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int txt; cin >> txt; int m = txt.size();
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
     
     return 0;
 }
